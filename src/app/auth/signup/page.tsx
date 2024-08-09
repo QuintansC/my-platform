f5ac1e3512126f8546787762ef6a047b9914036a
QuintansC <gustavosantos.2095@gmail.com>
2024-08-09 01:48:05 -0300

    fix: teste workflow

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -80,7 +80,7 @@ const SignUpComponent: React.FC = () => {
 
           <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
             <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
-              <span className="mb-1.5 block font-medium">Começe agora gratuitamente!</span>
+              <span className="mb-1.5 block font-medium">Começe agora, vamos!</span>
               <h1 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
                 Inscreva se no ArtFlame
               </h1>
